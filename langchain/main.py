d567ad2ff4dd7aa23d523e74871b0977b7814aab
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2026-02-15 19:02:20 +0000

    Starting Langchian Agentic v1.1

--- langchain/main.py
+++ langchain/main.py
@@ -1,8 +1,29 @@
 from langchain_openai import ChatOpenAI
-from app.utils import ai_settings
+from langchain.messages import HumanMessage
+from langchain.agents import create_agent
 
+from app.utils import ai_settings
+import streamlit as st
+
+st.title("AI Agent")
 
 llm_openai = ChatOpenAI(
-    api_key=ai_settings.OPENAI_API_KEY,
     model=ai_settings.OPENAI_MODEL,
+    api_key=ai_settings.OPENAI_API_KEY,
 )
+
+agent = create_agent(model=llm_openai)
+
+human_msg = st.chat_input("Please ask a question: ")
+
+
+def agent_response(human_msg: str):
+    for token, metadata in agent.stream(
+        {"messages": [HumanMessage(human_msg)]}, stream_mode="messages"
+    ):
+        if token.content:
+            yield token.content
+
+
+if human_msg:
+    st.write_stream(agent_response(human_msg))
