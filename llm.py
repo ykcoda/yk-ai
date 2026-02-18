182f13f808f4a3b50c7910e629db99c26a3f75d0
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2026-02-18 05:57:55 +0000

    add langchain messages

--- llm.py
+++ llm.py
@@ -1,20 +1,20 @@
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import PromptTemplate
+from langchain.messages import HumanMessage, SystemMessage
 
 load_dotenv()
 
 model = ChatOpenAI()
 
-prompt_msg = PromptTemplate(
-    input_variables=["topic"],
-    template="""Tell me a short joke about {topic}""",
-)
 
 # chain = model | output_parser
 
 # chain.invoke("who is currently leading the premier league")
 
-chain = prompt_msg | model
 
-chain.invoke({"topic": "football"})
+model.invoke(
+    [
+        HumanMessage("Tell me about programming"),
+        SystemMessage("You are helpfull assistance who tells joke"),
+    ]
+)
